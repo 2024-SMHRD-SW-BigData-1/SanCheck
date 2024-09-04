9e299355cac390e0707431bd6c554fda379bfd53
황세진 <stork0827@gmail.com>
2024-09-04 14:34:52 +0900

    Revert "Squashed commit of the following:"
    This reverts commit cb932c053cc0ca2a207ca86bd07207a36a3c4090.

--- lib/screen/delete_id.dart
+++ lib/screen/delete_id.dart
@@ -1,10 +1,8 @@
-import 'dart:convert';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sancheck/model/user_model.dart';
-import 'package:sancheck/screen/login_page.dart'; // 로그인 페이지를 가져옵니다.
-
-Dio dio = Dio();
+import 'package:sancheck/screen/login_page.dart';
+import 'package:sancheck/service/auth_service.dart';
 
 class DeleteId extends StatelessWidget {
   final TextEditingController _idController = TextEditingController();
@@ -14,76 +12,63 @@ class DeleteId extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF5F5F5), // 밝은 배경색 설정
+      backgroundColor: Color(0xFFF5F5F5), // 배경색 설정
       appBar: AppBar(
-        title: Text(
-          '회원탈퇴',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold), // 텍스트 스타일 적용
-        ),
-        centerTitle: true, // 제목 중앙 정렬
-        backgroundColor: Color(0xFFF5F5F5), // LoginSuccess의 상단바 배경색과 동일
-        elevation: 0, // 그림자 제거
-        iconTheme: IconThemeData(color: Colors.black), // 아이콘 색상 설정
+        title: Text('회원탈퇴'),
+        backgroundColor: Color(0xFFFFF5DA),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 20),
-            Container(
-              padding: const EdgeInsets.all(24),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.05),
-                    blurRadius: 10,
-                    offset: Offset(0, 5),
-                  ),
-                ],
+        padding: const EdgeInsets.all(16.0),
+        child: Container(
+          padding: const EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Color(0xFFD9D9D9)),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '회원탈퇴',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 24),
-                  _buildInputField('아이디', '아이디를 입력해 주세요.', _idController),
-                  SizedBox(height: 20),
-                  _buildInputField('비밀번호', '비밀번호를 입력해 주세요.', _pwController, obscureText: true),
-                  SizedBox(height: 20),
-                  _buildInputField('비밀번호 확인', '비밀번호를 다시 입력해 주세요.', _pwConfirmController, obscureText: true),
-                  SizedBox(height: 40),
-                  Align(
-                    alignment: Alignment.center,
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          handleDeleteId(context);
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFFE35154), // iOS Health 앱의 강렬한 경고 색상
-                          padding: EdgeInsets.symmetric(vertical: 16),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                        ),
-                        child: Text(
-                          '회원탈퇴',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
+              SizedBox(height: 24),
+              _buildInputField('아이디', '아이디를 입력해 주세요.', _idController),
+              SizedBox(height: 16),
+              _buildInputField('비밀번호', '비밀번호를 입력해 주세요.', _pwController),
+              SizedBox(height: 16),
+              _buildInputField('비밀번호 확인', '비밀번호를 다시 입력해 주세요.', _pwConfirmController),
+              Spacer(), // 버튼을 화면 하단으로 밀어줍니다.
+              Align(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: () {
+                    handleDeleteId(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFE35154), // 버튼 배경색
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
                     ),
                   ),
-                ],
+                  child: Text(
+                    '회원탈퇴',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -106,56 +91,31 @@ class DeleteId extends StatelessWidget {
       return;
     }
 
-    // 저장된 JSON 문자열을 읽어옴
-    String? userDataString = await storage.read(key: 'user');
+    // AuthService 인스턴스 생성
+    final AuthService _authService = AuthService();
 
-    if (userDataString != null) {
-      // JSON 문자열을 Map으로 디코딩
-      Map<String, dynamic> userDataMap = json.decode(userDataString);
+    // 로컬 저장소에서 사용자 데이터 읽기
+    UserModel? user = await _authService.readUserData();
 
-      // Map을 UserModel 객체로 변환
-      UserModel user = UserModel.fromJson(userDataMap);
+    if (user != null && userId == user.userId) {
+      // 회원 탈퇴 요청
+      bool isSuccess = await _authService.deleteUser(userId, userPw);
 
-      if (userId != user.userId) {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('입력한 아이디가 현재 로그인된 아이디와 다릅니다.')));
-        return;
-      }
-
-      try {
-        // 서버 통신
-        String url = "http://172.28.112.1:8000/user/handleDeleteId";
-        Response res = await dio.post(url, data: {
-          'user_id': userId,
-          'user_pw': userPw,
-        });
-        // 요청이 완료된 후에만 출력
-        print('Request URL: ${res.realUri}');
-        print('Status Code: ${res.statusCode}');
-
-        bool isSuccessed = res.data['success'];
-
-        if (isSuccessed) {
-          // 로컬저장소 값 삭제
-          await storage.delete(key: 'user');
-
-          Navigator.pushAndRemoveUntil(
-            context,
-            MaterialPageRoute(builder: (context) => LoginPage()),
-                (Route<dynamic> route) => false, // 모든 이전 화면을 제거
-          );
-        } else {
-          ScaffoldMessenger.of(context).hideCurrentSnackBar(); // 현재 스낵바 숨기기
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('회원탈퇴 실패'), backgroundColor: Colors.redAccent));
-        }
-      } catch (e) {
-        print('Error occurred: $e');
-        ScaffoldMessenger.of(context).hideCurrentSnackBar(); // 현재 스낵바 숨기기
+      if (isSuccess) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => LoginPage()),
+              (Route<dynamic> route) => false, // 모든 이전 화면을 제거
+        );
+      } else {
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('회원탈퇴 실패'), backgroundColor: Colors.redAccent));
       }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('입력한 아이디가 현재 로그인된 아이디와 다릅니다.'), backgroundColor: Colors.redAccent));
     }
   }
 
-  Widget _buildInputField(String label, String hintText, TextEditingController controller, {bool obscureText = false}) {
+  Widget _buildInputField(String label, String hintText, TextEditingController controller) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -170,16 +130,13 @@ class DeleteId extends StatelessWidget {
         SizedBox(height: 8),
         TextField(
           controller: controller,
-          obscureText: obscureText,
           decoration: InputDecoration(
             hintText: hintText,
             hintStyle: TextStyle(color: Color(0xFF9E9E9E)),
-            filled: true,
-            fillColor: Color(0xFFF5F5F5),
             contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
             border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide(color: Color(0xFFD9D9D9)),
             ),
           ),
         ),
--- lib/screen/user_profile.dart
+++ lib/screen/user_profile.dart
@@ -56,4 +56,3 @@ class UserProfile extends StatelessWidget {
     );
   }
 }
-
